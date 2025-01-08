7ae2edff293faaa5be315fee01483ee42ca71392
Leigh Oliver <leighcharlesoliver@gmail.com>
2025-01-09 08:37:46 +1100

    Incomplete YUYV -> JPEG conversion code

---
+++ shell.nix
@@ -0,0 +1,44 @@
+{ nixpkgs ? import <nixpkgs> {}}:
+let 
+  #rustOverlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  rustOverlay = nixpkgs.fetchFromGitHub {
+    owner = "oxalica";
+    repo = "rust-overlay";
+    rev = "a0b81d4fa349d9af1765b0f0b4a899c13776f706";
+    hash = "sha256-IKrk7RL+Q/2NC6+Ql6dwwCNZI6T6JH2grTdJaVWHF0A=";
+  };
+
+  pkgs = import <nixpkgs> {
+    overlays = [ (import rustOverlay) ];
+  };
+
+  rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+in
+  pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } rec {
+    buildInputs = [ rust ] ++ (with pkgs; [
+      #bacon 
+      #gcc 
+      #rust-analyzer
+      #stdenv.cc 
+      #systemd
+      pkg-config
+      glib
+      zlib
+      libusb1
+      
+      # cross-rs needs this binary
+      #rustup
+
+      # for libcamera-rs
+      libcamera
+      libclang
+    ]);
+
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+
+    shellHook = ''
+        export PS1="''${debian_chroot:+($debian_chroot)}\[\033[01;39m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "
+        export PS1="(nix-rs)$PS1"
+        export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}"
+    '';
+  }
---
+++ std-example/src/main.rs
@@ -0,0 +1,194 @@
+use embassy_executor::Spawner;
+use embassy_time::{Duration,Timer};
+use log::*;
+use libcamera::{camera_manager::CameraManager, logging::LoggingLevel, stream::StreamRole};
+
+use libcamera::{
+    camera::CameraConfigurationStatus,
+    framebuffer::AsFrameBuffer,
+    framebuffer_allocator::{FrameBuffer, FrameBufferAllocator},
+    framebuffer_map::MemoryMappedFrameBuffer,
+    pixel_format::PixelFormat,
+    request::ReuseFlag,
+    properties,
+};
+
+// drm-fourcc does not have MJPEG type yet, construct it from raw fourcc identifier
+//const PIXEL_FORMAT_MJPEG: PixelFormat = PixelFormat::new(u32::from_le_bytes([b'M', b'J', b'P', b'G']), 0);
+const PIXEL_FORMAT_MJPEG: PixelFormat = PixelFormat::new(u32::from_le_bytes([b'Y', b'U', b'Y', b'V']), 0);
+
+#[embassy_executor::task]
+async fn task_camera_capture(filename : String) {
+    let mgr = CameraManager::new().unwrap();
+    mgr.log_set_level("Camera", LoggingLevel::Error);
+
+    let cameras = mgr.cameras();
+
+    let cam = cameras.get(0).expect("No cameras found");
+
+    info!(
+        "Using camera: {}",
+        *cam.properties().get::<properties::Model>().unwrap()
+    );
+
+    let mut cam = cam.acquire().expect("Unable to acquire camera");
+
+    // This will generate default configuration for each specified role
+    //let mut cfgs = cam.generate_configuration(&[StreamRole::Raw]).unwrap();
+    let mut cfgs = cam.generate_configuration(&[StreamRole::StillCapture]).unwrap();
+    //let mut cfgs = cam.generate_configuration(&[StreamRole::VideoRecording]).unwrap();
+    //let mut cfgs = cam.generate_configuration(&[StreamRole::ViewFinder]).unwrap();
+
+    // Use MJPEG format so we can write resulting frame directly into jpeg file
+    cfgs.get_mut(0).unwrap().set_pixel_format(PIXEL_FORMAT_MJPEG);
+
+    info!("Generated config: {:#?}", cfgs);
+
+    match cfgs.validate() {
+        CameraConfigurationStatus::Valid => info!("Camera configuration valid!"),
+        CameraConfigurationStatus::Adjusted => info!("Camera configuration was adjusted: {:#?}", cfgs),
+        CameraConfigurationStatus::Invalid => panic!("Error validating camera configuration"),
+    }
+
+    // Ensure that pixel format was unchanged
+    assert_eq!(
+        cfgs.get(0).unwrap().get_pixel_format(),
+        PIXEL_FORMAT_MJPEG,
+        "MJPEG is not supported by the camera"
+    );
+
+    cam.configure(&mut cfgs).expect("Unable to configure camera");
+
+    let mut alloc = FrameBufferAllocator::new(&cam);
+
+    // Allocate frame buffers for the stream
+    let cfg = cfgs.get(0).unwrap();
+    let stream = cfg.stream().unwrap();
+    let buffers = alloc.alloc(&stream).unwrap();
+    info!("Allocated {} buffers", buffers.len());
+
+    // Convert FrameBuffer to MemoryMappedFrameBuffer, which allows reading &[u8]
+    let buffers = buffers
+        .into_iter()
+        .map(|buf| MemoryMappedFrameBuffer::new(buf).unwrap())
+        .collect::<Vec<_>>();
+
+    // Create capture requests and attach buffers
+    let mut reqs = buffers
+        .into_iter()
+        .map(|buf| {
+            let mut req = cam.create_request(None).unwrap();
+            req.add_buffer(&stream, buf).unwrap();
+            req
+        })
+        .collect::<Vec<_>>();
+
+    // Completed capture requests are returned as a callback
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    cam.on_request_completed(move |req| {
+        tx.send(req).unwrap();
+    });
+
+    cam.start(None).unwrap();
+
+    
+    loop {
+        // Multiple requests can be queued at a time, but for this example we just want a single frame.
+        cam.queue_request(reqs.pop().unwrap()).unwrap();
+
+        info!("Waiting for camera request execution");
+        let mut req = rx.recv_timeout(Duration::from_secs(1).into()).expect("Camera request failed");
+        info!("Camera request {:?} completed!", req);
+        info!("Metadata: {:#?}", req.metadata());
+        // Get framebuffer for our stream
+        let framebuffer: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
+        info!("FrameBuffer metadata: {:#?}", framebuffer.metadata());
+        // MJPEG format has only one data plane containing encoded jpeg data with all the headers
+        let planes = framebuffer.data();
+        //info!("{:?}", planes);
+
+        let jpeg_data = planes.get(0).unwrap();
+        let jpeg_len = framebuffer.metadata().unwrap().planes().get(0).unwrap().bytes_used as usize; // Actual JPEG-encoded data will be smalled than framebuffer size, its length can be obtained from metadata.
+                                                                                                     //
+        let (width, height) = (1920, 1080);
+
+        // Our source image, an RGB buffer
+        let rgb_image = vec![0u8; PixelFormat::RGB.buffer_size(width, height)];
+
+        // Create the image we're converting from
+        let source = Image::from_buffer(
+            PixelFormat::RGB,
+            &rgb_image[..], // RGB only has one plane
+            None, // No need to define strides if there's no padding between rows
+            width,
+            height,
+            ColorInfo::RGB(RgbColorInfo {
+                transfer: ColorTransfer::Linear,
+                primaries: ColorPrimaries::BT709,
+            }),
+        ).unwrap();
+
+        // Create the image buffer we're converting to
+        let mut destination = Image::blank(
+            PixelFormat::NV12, // We're converting to NV12
+            width,
+            height,
+            ColorInfo::YUV(YuvColorInfo {
+                space: ColorSpace::BT709,
+                transfer: ColorTransfer::Linear,
+                primaries: ColorPrimaries::BT709,
+                full_range: false,
+            }),
+        );
+
+        // Now convert the image data
+        convert_multi_thread(
+            &source,
+            &mut destination,
+        ).unwrap();
+
+
+        let fname = format!("{}_{}.raw",&filename,req.sequence());
+        std::fs::write(&fname, &jpeg_data[..jpeg_len]).unwrap();
+        info!("Written {} bytes to {}", jpeg_len, &fname);
+
+        // Push request back onto queue and go again
+        Timer::after_millis(1000).await;
+
+        req.reuse(ReuseFlag::REUSE_BUFFERS);
+        reqs.push(req);
+
+    }
+}
+
+
+#[embassy_executor::task]
+async fn task_ticker() {
+    loop {
+        info!("tick");
+        Timer::after_millis(500).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn task_beeper() {
+    loop {
+        info!("beep");
+        Timer::after_millis(2000).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let filename = std::env::args().nth(1).expect("Usage ./app <filename.jpg>");
+
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .format_timestamp_nanos()
+        .init();
+
+    spawner.spawn(task_ticker()).unwrap();
+    spawner.spawn(task_beeper()).unwrap();
+    spawner.spawn(task_camera_capture(filename)).unwrap();
+}
